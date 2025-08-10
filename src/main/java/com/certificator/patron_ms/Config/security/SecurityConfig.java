c3a807a92e6cfd669ad365474994f918b4e89bdc
oscar-co <carrero_o@outlook.com>
2025-08-10 23:14:58 +0200

    Logging mode debug

--- src/main/java/com/certificator/patron_ms/Config/security/SecurityConfig.java
+++ src/main/java/com/certificator/patron_ms/Config/security/SecurityConfig.java
@@ -54,7 +54,12 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://ocoprojects.online:8080", "http://ocoprojects.online"));
+        configuration.setAllowedOrigins(List.of(
+            "http://localhost:4200", 
+            "http://ocoprojects.online:8080", 
+            "http://ocoprojects.online",
+            "https://ocoprojects.online"
+        ));        
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
